752565600812d78655987004e899caf36d0138a5
Brandon Segers <brandon.segers@outlook.fr>
2022-06-30 15:50:21 +0200

    add hour

--- workee/src/components/home/Home.tsx
+++ workee/src/components/home/Home.tsx
@@ -1,13 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Logo from '../../assets/logo/logo-workee.png';
-import { BaseDirectory, createDir, readTextFile, writeTextFile } from "@tauri-apps/api/fs";
 import './Home.css';
 
 function Home() {
+    const [dateNow, setDateNow] = useState("");
+    
+    useEffect(() => {
+        init();
+    });
 
+    const getDate = () => {
+        let date = new Date();
+        let hour = (date.getHours() < 10 ? '0' : '') + date.getHours();
+        let minute = (date.getMinutes() < 10 ? '0' : '') + date.getMinutes();
+        let second = (date.getSeconds() < 10 ? '0' : '') + date.getSeconds();
+        console.log(`${hour}:${minute}:${second}`)
+        return `${hour}:${minute}:${second}`;
+    }
+
+    const init = () => {
+        setDateNow(getDate());
+        setInterval(() => {
+            setDateNow(getDate());
+        }, 1000);
+    }
     return (
-        <div className="background">
-            <p>Salut</p>
+        <div className="background-white">
+            <nav className="navbar bg-workee">
+                <div className="container-fluid">
+                    <img src={Logo} alt="" className="d-inline-block align-text-top logoMenu"/>
+                    <ul className="navbar-nav hour">
+                    <li className="nav-item">
+                        <span className="date">{dateNow}</span>
+                    </li>
+                    </ul>
+                    <div className="navbar-brand">
+                    </div>
+                </div>
+            </nav>
         </div>
     );
 }
