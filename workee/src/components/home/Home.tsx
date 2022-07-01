d2d888fea0592bd202a8f845a63e9e931bd475d1
Brandon Segers <brandon.segers@outlook.fr>
2022-07-01 09:31:24 +0200

    add menu

--- workee/src/components/home/Home.tsx
+++ workee/src/components/home/Home.tsx
@@ -38,6 +38,41 @@ function Home() {
                     </div>
                 </div>
             </nav>
+            <div className="container-fluid">
+            <div className="row justify-content-center">
+                <div className="col-3 sphere">
+                    <span className=""></span>
+                </div>
+                <div className="col-3 sphere">
+                    <span></span>
+                </div>
+                <div className="col-3 sphere">
+                    <span></span>
+                </div>
+                <div className="col-3 sphere">
+                    <span></span>
+                </div>
+            </div>
+            <div className="row justify-content-center">
+                <div className="col-3 sphere">
+                    <span className=""></span>
+                </div>
+                <div className="col-3 sphere">
+                    <span></span>
+                </div>
+                <div className="col-3 sphere">
+                    <span></span>
+                </div>
+                <div className="col-3 sphere">
+                    <span></span>
+                </div>
+            </div>
+            <div className="row justify-content-center">
+                <div className="col-12">
+                    <span className=""></span>
+                </div>
+            </div>
+            </div>
         </div>
     );
 }
