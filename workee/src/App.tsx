254a9c863eb2e1c12793e2c2339dbd2711b8a8de
teo-mtn <teo.martin@epitech.eu>
2022-07-22 14:35:09 +0200

    adding route for login page

--- workee/src/App.tsx
+++ workee/src/App.tsx
@@ -6,10 +6,12 @@ import Loading from './components/loading/Loading';
 import Base from './components/base/Base';
 import Home from './components/home/Home';
 import Configuration from './components/base/configuration/Configuration';
+import Login from './components/login/Login';
 function App() {
   return (
     <div className="App">
       <Routes>
+        <Route path="/login" element={< Login/>} />
         <Route path="/" element={< Loading/>} />
         <Route path="/w" element={< Base/>} >
           <Route path="test" element={<Home/>} />
