974513591f39c6283003f1fca16a65b741eb0e1d
teo-mtn <teo.martin@epitech.eu>
2022-07-22 10:15:36 +0200

    adding login components

---
+++ workee/src/components/login/Login.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import { Button } from '../ui/button/Button';
+
+export default class Login extends Component {
+    render() {
+        return(
+            <Button>"Coucou"</Button>
+        )};
+}
