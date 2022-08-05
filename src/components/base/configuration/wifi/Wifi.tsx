772a6b84777dd1a0c4da4ffd9e76e3358a93e1ba
Brandon Segers <brandon.segers@outlook.fr>
2022-08-05 15:09:02 +0200

    test

--- src/components/base/configuration/wifi/Wifi.tsx
+++ src/components/base/configuration/wifi/Wifi.tsx
@@ -16,7 +16,7 @@ function Wifi(props: any) {
     const [password, setPassword] = useState("");
     const [wifiConnectLoading, setWifiConnectLoading] = useState(false);
     const dispatch = useDispatch();
-
+    const [html, setHtml] = useState([<div></div>]);
     const [expanded, setExpanded] = React.useState<string | false>(false);
 
   const handleChange =
@@ -27,6 +27,9 @@ function Wifi(props: any) {
     useEffect(() => {
         getWifiList();
     }, []);
+    useEffect(() => {
+        renderHtmlWifi();
+    }, [loading]);
 
     const connectWifi = async () => {
         var win = window as any;
@@ -60,57 +63,49 @@ function Wifi(props: any) {
         setLoading(false);
     }
 
-    const renderHtmlWifi = () => {
-        if (loading) {
-            return (
-                <h2>Recherche WiFi <i className="fa-solid fa-circle-notch fa-spin"></i></h2>
-            );
-        } else {
-            const htmlWifi = wifiList.map((wifi: any) =>
-            <div>
-                <Accordion disableGutters expanded={expanded === wifi.ssid} onChange={handleChange(wifi.ssid)}>
-                <AccordionSummary
-                    expandIcon={<ExpandMoreIcon />}
-                    aria-controls="panel1bh-content"
-                    id="panel1bh-header"
-                >
-                <Typography>
-                    <span className="wifi-item-ssid">{wifi.ssid} {wifi.signal} dB</span>
-                </Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                <Typography>
-                    <div className="row">
-                        <label className="col-6">
-                            <Input id={wifi.ssid} label={"Clé de sécurité " + wifi.flags} setCustom={setPassword} value={password} onChange={onChangePassword} type="password" name="password" />
-                        </label>
-                        <div className="col-6">
-                                <Button disabled={password.length < 5} className="test" click={connectWifi} type="workee">
-                                    {wifiConnectLoading ? <i className="fa-solid fa-circle-notch fa-spin"></i> : "Connexion"}
-                                </Button>
-                        </div>
-                    </div>
-                </Typography>
-                </AccordionDetails>
-            </Accordion>
-            <br/>
-            </div>
-
-            );
-            return (
-                <div className="accordion wifiList window" id="accordion">
-                    {htmlWifi}
-                </div>
-            )
-        }
+    const renderHtmlWifi = async () => {
     }
     return (
         <div className={props}>
             <div className="container-fluid text-center"><br/>
                 <div className="">
-                    {
-                        renderHtmlWifi()
-                    }
+                    <div className="accordion wifiList window" id="accordion">
+                        {loading &&
+                            <h2>Recherche WiFi <i className="fa-solid fa-circle-notch fa-spin"></i></h2>
+                        }
+                        {!loading &&
+                        wifiList.map((wifi: any) =>
+                        <div>
+                            <Accordion disableGutters expanded={expanded === wifi.ssid} onChange={handleChange(wifi.ssid)}>
+                            <AccordionSummary
+                                expandIcon={<ExpandMoreIcon />}
+                                aria-controls="panel1bh-content"
+                                id="panel1bh-header"
+                            >
+                            <Typography>
+                                <span className="wifi-item-ssid">{wifi.ssid} {wifi.signal} dB</span>
+                            </Typography>
+                            </AccordionSummary>
+                            <AccordionDetails>
+                            <Typography>
+                                <div className="row">
+                                    <label className="col-6">
+                                        <Input id={wifi.ssid} label={"Clé de sécurité " + wifi.flags} setCustom={setPassword} value={password} onChange={onChangePassword} type="password" name="password" />
+                                    </label>
+                                    <div className="col-6">
+                                            <Button disabled={password.length < 5} className="test" click={connectWifi} type="workee">
+                                                {wifiConnectLoading ? <i className="fa-solid fa-circle-notch fa-spin"></i> : "Connexion"}
+                                            </Button>
+                                    </div>
+                                </div>
+                            </Typography>
+                            </AccordionDetails>
+                        </Accordion>
+                        <br/>
+                        </div>
+                
+                        )}
+                    </div>
                 </div>
                 <hr/>
                 <footer className="row footerConfig">
