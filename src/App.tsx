05f36128a9e4961eae709c6734450b52e83336ed
mteon <teo.martin@epitech.eu>
2022-09-16 14:19:42 +0200

    Finalized Login Component without the custom input (+token + error popup

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,9 @@ import Configuration from './components/base/configuration/Configuration';
 import Keyboard from "react-simple-keyboard"
 import { useSelector } from 'react-redux';
 import Login from './components/login/Login';
+import axios from 'axios';
+
+axios.defaults.baseURL = "https://workee-back.brangers.eu/api";
 
 function App() {
   const keyboard = useSelector((state: any) => {
--- src/components/ui/formInput/LoginForm.tsx
+++ src/components/ui/formInput/LoginForm.tsx
@@ -1,45 +1,62 @@
-import { useState } from 'react';
-//import { useNavigate } from 'react-router-dom';
-import { Button } from '../button/Button';
+import { useCallback, useState } from 'react';
+import { Button } from '../../../ui-kit';
 import { Input } from '../input/Input';
 import './LoginForm.css';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import Cookies from 'universal-cookie';
+import { useSnackbar } from 'notistack';
+
 
 export const LoginForm = () => {
-    //const navigate = useNavigate();
+    const navigate = useNavigate();
+    const { enqueueSnackbar } = useSnackbar();
 
     const [emailValue, setEmailValue]= useState<string>();
     const [passwordValue, setPasswordValue] = useState<string>();
 
-    const handleChangeEmail = (event:any) => {
+    const handleChangeEmail = useCallback((event:any) => {
         const target = event.target;
-        const email = target.email;
+        const email = target.value;
         setEmailValue(email);
-        console.log(emailValue);
-    }
+    }, []);
 
-    const handleChangePassword = (event:any) => {
+    const handleChangePassword = useCallback((event:any) => {
         const target = event.target;
-        const password = target.passwordValue;
+        const password = target.value;
         setPasswordValue(password);
-        console.log(passwordValue);
-    }
+    }, [])
 
     const handleSubmit = () =>  {
-        
+        axios.post('/login', 
+        {
+            email: emailValue,
+            password: passwordValue
+        }).then(function (response) {
+            const cookie = new Cookies();
+            cookie.set(response.data.token, { path: '/' });
+            navigate("/w/home");
+        }).catch(function(error) {
+            enqueueSnackbar(error.response.data.message, {variant: 'error'});
+        });
     }
     
     return (
-        <form onSubmit={handleSubmit}>
+        <form>
             <Input placeholder="email"
-            setCustom={setEmailValue}
+            id="loginEmail"
+            //setCustom={handleChangeEmail}
             value={emailValue}
             onChange={handleChangeEmail}
             />
             <Input placeholder="password"
-            setCustom={setPasswordValue}
+            id="loginPassword"
+            type="password"
+            //setCustom={handleChangePassword}
             value={passwordValue}
-            onChange={handleChangePassword} />
-            <Button className="workee" type="submit">Login</Button>
+            onChange={handleChangePassword} 
+            />
+            <Button className="workee" onClick={handleSubmit}>Login</Button>
         </form>
     );
 }
