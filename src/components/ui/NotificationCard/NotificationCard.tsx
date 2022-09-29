4d65638eecbd15dc1e4b2ac767a8ed6ebc3e25cf
mteon <teo.martin@epitech.eu>
2022-09-29 15:15:31 +0200

    Changing NotificationCard design

--- src/components/ui/NotificationCard/NotificationCard.tsx
+++ src/components/ui/NotificationCard/NotificationCard.tsx
@@ -1,20 +1,29 @@
-import { Avatar, Box, Stack, Typography } from "@mui/material"
+import { Avatar, Box, Card, Stack, Typography } from "@mui/material"
+import { format } from 'date-fns'
+import styled from '@emotion/styled';
+import { padding } from "@mui/system";
+
+const BoldTypo = styled(Typography)`
+    font-weight: bold
+`
+var date = new Date("2022-09-29T14:26:09+02:00");
+
 
 export const NotificationCard = () => {
     return (
-        <Box>
-            <Box>
-                <Typography fontSize='1em'>Debi mérite le plus grand des respects woulah</Typography>
-            </Box>
-            <Box>
-                <Stack justifyContent={"flex-start"}>
-                    <Avatar>TM</Avatar>
-                    <Typography fontSize='1em'>Téo Martin</Typography>
+        <Card elevation={5}>
+            <Box padding={2}>
+                <Stack justifyContent={"space-between"} direction={"row"}>
+                    <Stack justifyContent={"flex-start"} direction={"row"}>
+                        <Avatar>T</Avatar>
+                        <Stack direction={"column"} textAlign="left" marginLeft={2}>
+                            <BoldTypo fontSize='1em' >Téo Martin</BoldTypo>
+                            <Typography fontSize='1em'>{format(date,"dd MMMM yyyy")}</Typography>
+                        </Stack>
+                    </Stack>
+                    <Typography fontSize='1em'>Lorem Ipsum is simply dummy text of the printing and t</Typography>
                 </Stack>
-                <Stack justifyContent={"flex-end"}>
-                    <Typography fontSize='1em'>Critique</Typography>
-                </Stack>
             </Box>
-        </Box>
+        </Card>
     )
 }
