2e117870a404e851371f3b54c4d364bf076e02df
mteon <teo.martin@epitech.eu>
2022-09-29 16:58:26 +0200

    Final Notification Page

--- src/components/notifications/Notifications.tsx
+++ src/components/notifications/Notifications.tsx
@@ -1,12 +1,35 @@
 import { Container, Stack, Typography } from '@mui/material';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Config } from '../../Config';
 import {NotificationCard} from '../ui/NotificationCard/NotificationCard'
+import type { Notification } from './INotification';
+
+const token = localStorage.getItem("token");
+
+const config = {
+    headers: { Authorization: `Bearer ${token}` }
+};
 
 export const Notifications = () => {
+    const [notifications, setNotifications] = useState<Notification[]>();
+   
+    async function init() {
+        const notif = await axios.get("/notifications", config);
+        setNotifications(notif.data);
+    }
+
+    useEffect(() => {
+        init()
+    }, [])
+
     return(
         <Container>
             <Typography marginBottom={2} marginTop={2} variant='h1' fontSize='3em'>Vos Notifications</Typography>
             <Stack direction="column">
-                <NotificationCard />
+            {notifications?.map((notification) => {
+                return(<NotificationCard notification={notification} key={notification.id}/>);
+            })} 
             </Stack>
         </Container>
     );
