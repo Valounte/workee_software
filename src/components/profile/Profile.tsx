67461b6ab7e295597ed3507ab19b846ac80c01d6
mteon <teo.martin@epitech.eu>
2022-09-30 16:03:35 +0200

    Profile page retrieving data from user, missing only design

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,7 +1,31 @@
+import { Avatar, Container } from '@mui/material';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { IUser } from './IUser';
+
+
+const token = localStorage.getItem("token");
+const config = { headers: { Authorization: `Bearer ${token}` } };
+
+    
 export const Profile = () => {
+    const [user, setUser] = useState<IUser>();
+
+    async function init() {
+        const me = await axios.get("/me", config);
+        setUser(me.data);
+    }
+
+    useEffect(() => {
+        init()
+    }, [])
+
+
     return (
-        <p>Hello World</p>
+        <Container>
+            <Avatar alt={user?.firstname} src={user?.picture} />
+            <p>{user?.email}</p>
+        </Container>
     );
 }
-
 export default Profile;
