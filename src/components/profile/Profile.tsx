1dbcdfb06dc3f86cf9f35358d8483cd1c4b0ce04
mteon <teo.martin@epitech.eu>
2022-10-06 10:03:23 +0200

    adding disconnection button

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,8 +1,11 @@
-import { Avatar, Container } from '@mui/material';
+import { Avatar, Button, Container, Stack, Typography } from '@mui/material';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { IUser } from './IUser';
+import { useNavigate } from 'react-router-dom';
 
+// eslint-disable-next-line react-hooks/rules-of-hooks
+const navigate = useNavigate();
 
 const token = localStorage.getItem("token");
 const config = { headers: { Authorization: `Bearer ${token}` } };
@@ -23,8 +26,26 @@ export const Profile = () => {
 
     return (
         <Container>
-            <Avatar alt={user?.firstname} src={user?.picture} />
-            <p>{user?.email}</p>
+            <Typography marginBottom={2} marginTop={2} variant='h3' fontSize='3em'>Mon Profil</Typography>
+            <Stack direction={"row"} spacing={{sm: 5}}>
+                <Avatar alt={user?.firstname} src={user?.picture} />
+                <Stack direction={"column"} spacing={{sm: 1}}>
+                    <Typography variant='h4' fontSize='1em'>{user?.email}</Typography>
+                    <Typography variant='h4' fontSize='1em'>{user?.firstname + " " + user?.lastname}</Typography>
+                </Stack>
+                <Stack direction={"column"}>
+                    <Typography variant='h4' fontSize='1.5em'>Entreprise</Typography>
+                    <Typography variant='h4' fontSize='1em'>{user?.company.name}</Typography>
+                    <Typography variant='h4' fontSize='1.5em'>Equipe</Typography>
+                    <Typography variant='h4' fontSize='1em'>{user?.teams[0].name}</Typography>
+                    <Typography variant='h4' fontSize='1.5em'>Job</Typography>
+                    <Typography variant='h4' fontSize='1em'>{user?.job.name}</Typography>
+                </Stack>
+                <Button onClick={() => {
+            localStorage.setItem("token", "");
+            navigate("/w/login")
+        }}>Se Deconnecter</Button>
+            </Stack>
         </Container>
     );
 }
