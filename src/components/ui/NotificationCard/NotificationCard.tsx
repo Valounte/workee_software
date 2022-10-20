d5b7ef88e81eac8a126dc8e62d9a774ecbe7c512
mteon <teo.martin@epitech.eu>
2022-10-20 10:21:20 +0200

    Adding margin to props in order to make card spacing

--- src/components/ui/NotificationCard/NotificationCard.tsx
+++ src/components/ui/NotificationCard/NotificationCard.tsx
@@ -2,6 +2,7 @@ import { Avatar, Box, Card, Stack, Typography } from "@mui/material"
 import { format } from 'date-fns'
 import styled from '@emotion/styled';
 import { Notification } from "../../notifications/INotification";
+import { AlertBadge } from "../AlertBadge/AlertBadge";
 
 const BoldTypo = styled(Typography)`
     font-weight: bold
@@ -9,25 +10,35 @@ const BoldTypo = styled(Typography)`
 
 interface NotificationCardProps {
     notification: Notification
+    marginBot: string
 }
 
 export const NotificationCard = (props: NotificationCardProps) => {
-    const {notification} = props;
+    const {notification, marginBot} = props;
     const {message, sentAt, senderFirstname, senderLastname, alertLevel} = notification;
     return (
-        <Card elevation={5}>
-            <Box padding={2}>
-                <Stack direction={"row"} spacing={{sm: 15}}>
-                    <Stack justifyContent={"flex-start"} direction={"row"}>
-                        <Avatar>{senderFirstname.substring(0,1)}</Avatar>
-                        <Stack direction={"column"} textAlign="left" marginLeft={2}>
-                            <BoldTypo fontSize='1em'>{senderFirstname + " " + senderLastname + " - " + alertLevel}</BoldTypo>
-                            <Typography fontSize='0.75em'>{format(new Date(sentAt),"dd/MM/yyyy HH:mm:ss")}</Typography>
+        <Box marginBottom={marginBot}>
+            <Card elevation={5}>
+                <Box padding={2}>
+                    <Stack direction={"row"}>
+                        <Stack direction={"column"} spacing={{sm: -2}}>
+                            <Stack justifyContent={"flex-start"} direction={"row"}>
+                                <Avatar>{senderFirstname.substring(0,1)}</Avatar>
+                                <Stack direction={"row"} marginLeft={2}>
+                                    <BoldTypo fontSize='1em'>{senderFirstname + " " + senderLastname}</BoldTypo>
+                                    <Typography fontSize='0.75em'>{format(new Date(sentAt),"dd/MM/yyyy HH:mm:ss")}</Typography>
+                                </Stack>
+                            </Stack>
+                            <Stack  justifyContent={"flex-start"}>
+                                <Typography fontSize='1em' >{message}</Typography>
+                            </Stack>
                         </Stack>
-                    </Stack>
-                    <Typography fontSize='1em'>{message}</Typography>
-                </Stack>
+                        <Stack justifyContent={"flex-end"} marginLeft={40}>
+                            <AlertBadge alertType={alertLevel}/>
+                   </Stack>
+               </Stack>
             </Box>
         </Card>
+    </Box>
     )
 }
